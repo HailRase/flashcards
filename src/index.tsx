aafed815b94bc7c31e86078327a311b4c93c1a63
Юра <yurii_klimko@mail.ru>
2022-01-22 16:13:03 +0300

    init commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './s1-main/m1-ui/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
