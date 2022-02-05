d7bd395a0205f14087c4a0c2e1e69c94e077bcd8
mykolao <mykolaoleskiv@gmail.com>
2022-02-05 19:28:26 +0200

    create password reducer

--- src/s1-main/m2-bll/password-reducer.ts
+++ src/s1-main/m2-bll/password-reducer.ts
@@ -1,6 +1,50 @@
-export const passwordReducer = (state: any = {}, action: any): any => {
-    switch (action.type) {
-        default:
-            return state
+import {Dispatch} from "redux";
+import {PasswordAPI} from "../m3-dal/api";
+
+// ACTIONS
+const setResetPasswordStatus = (
+    status: ResetPasswordStatus
+) => {
+    return {
+        type: "PASSWORD/SET_RESET_PASSWORD_STATUS",
+        payload: status,
+    } as const;
+};
+
+type SetResetPasswordStatusAction = ReturnType<typeof setResetPasswordStatus>;
+
+// THUNKS
+const changePassword = (password: string, token: string) => {
+    return async (dispatch: Dispatch<SetResetPasswordStatusAction>) => {
+        const res = await PasswordAPI.changePassword(password, token);
+        const action = res.data.info ? setResetPasswordStatus("success") : setResetPasswordStatus("error")
+        dispatch(action);
     }
 }
+
+// REDUCER
+const initialState: PasswordReducerType = {
+    resetPasswordStatus: "init",
+};
+
+export const passwordReducer = (
+    state = initialState,
+    action: Action
+): PasswordReducerType => {
+    switch (action.type) {
+        case "PASSWORD/SET_RESET_PASSWORD_STATUS": {
+            return {...state, resetPasswordStatus: action.payload};
+        }
+        default:
+            return state;
+    }
+};
+
+// TYPES
+type ResetPasswordStatus = "init" | "success" | "error";
+
+type PasswordReducerType = {
+    resetPasswordStatus: ResetPasswordStatus;
+};
+
+type Action = SetResetPasswordStatusAction;
