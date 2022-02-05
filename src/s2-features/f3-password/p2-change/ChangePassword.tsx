e566764e3c2bfd25561e2663d5bfaf78c83fb75c
mykolao <mykolaoleskiv@gmail.com>
2022-02-05 18:41:03 +0200

    jsx layout

--- src/s2-features/f3-password/p2-change/ChangePassword.tsx
+++ src/s2-features/f3-password/p2-change/ChangePassword.tsx
@@ -1,9 +1,22 @@
+import SuperInputText from "../../../s1-main/m1-ui/common/c1-SuperInputText/SuperInputText";
+import SuperButton from "../../../s1-main/m1-ui/common/c2-SuperButton/SuperButton";
+import s from "./ChangePassword.module.css";
+
 const ChangePassword = () => {
-    return (
-        <div>
-            <h1>CHANGE PASSWORD PAGE!</h1>
-        </div>
-    );
+  return (
+    <div className={s.wrapper}>
+      <div className={s.container}>
+        <h2 className={s.logo}>it-incubator</h2>
+        <h2 className={s.title}>Create New Password</h2>
+        <SuperInputText type="password" />
+        {/* <p className={s.desc}>
+          Create new password and we will send you further
+          instructions to email
+        </p> */}
+        <SuperButton>Change Password</SuperButton>
+      </div>
+    </div>
+  );
 };
 
 export default ChangePassword;
