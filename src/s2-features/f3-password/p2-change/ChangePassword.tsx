ec81fc7f53f80cb28e9ae3daec840855770a4dd2
mykolao <mykolaoleskiv@gmail.com>
2022-02-05 18:50:08 +0200

    create local state

--- src/s2-features/f3-password/p2-change/ChangePassword.tsx
+++ src/s2-features/f3-password/p2-change/ChangePassword.tsx
@@ -1,19 +1,34 @@
+import { ChangeEvent, useState } from "react";
 import SuperInputText from "../../../s1-main/m1-ui/common/c1-SuperInputText/SuperInputText";
 import SuperButton from "../../../s1-main/m1-ui/common/c2-SuperButton/SuperButton";
 import s from "./ChangePassword.module.css";
 
 const ChangePassword = () => {
+  const [newPassword, setNewPassword] = useState("");
+
+  const onNewPasswordChange = (
+    e: ChangeEvent<HTMLInputElement>
+  ) => {
+    setNewPassword(e.currentTarget.value);
+  };
+
+  const onSubmit = () => {
+    alert(`New Password Set: ${newPassword}`);
+  };
+
   return (
     <div className={s.wrapper}>
       <div className={s.container}>
         <h2 className={s.logo}>it-incubator</h2>
         <h2 className={s.title}>Create New Password</h2>
-        <SuperInputText type="password" />
-        {/* <p className={s.desc}>
-          Create new password and we will send you further
-          instructions to email
-        </p> */}
-        <SuperButton>Change Password</SuperButton>
+        <SuperInputText
+          type="password"
+          value={newPassword}
+          onChange={onNewPasswordChange}
+        />
+        <SuperButton onClick={onSubmit}>
+          Change Password
+        </SuperButton>
       </div>
     </div>
   );
