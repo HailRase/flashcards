1ac6951a9ee2e2a2cf6255de4f0c96a7057c14e8
mykolao <mykolaoleskiv@gmail.com>
2022-02-12 20:25:00 +0200

    move Routes to App component

--- src/s1-main/m1-ui/App.tsx
+++ src/s1-main/m1-ui/App.tsx
@@ -1,12 +1,21 @@
 import React, {useEffect} from 'react';
 import './App.css';
-import Header from "./header/Header";
-import {HashRouter} from "react-router-dom";
-import Main from './ main/Main';
+import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
 import {Provider, useDispatch} from 'react-redux';
 import store, {useAppSelector} from "../m2-bll/store";
 import {initializeApp} from "../m2-bll/app-reducer";
+
+import { PATH } from './routes/routes';
+
 import Preloader from "./common/Preloader/Preloader";
+import Header from "./header/Header";
+import Test from '../../s2-features/f0-test/Test';
+import Profile from '../../s2-features/f2-profile/Profile';
+import Login from '../../s2-features/f1-auth/a1-login/Login';
+import Register from '../../s2-features/f1-auth/a2-register/Register';
+import PasswordRecovery from '../../s2-features/f3-password/p1-recovery/PasswordRecovery';
+import ChangePassword from '../../s2-features/f3-password/p2-change/ChangePassword';
+import E404 from '../../s2-features/f4-E404/E404';
 
 const App = () => {
     const initialized = useAppSelector(state => state.app.initialized)
@@ -21,7 +30,17 @@ const App = () => {
     return (
         <div className="App">
             <Header/>
-            <Main/>
+            <Routes>
+                <Route path='/' element={<Navigate to={PATH.PROFILE}/>}/>
+                <Route path={PATH.TEST} element={<Test/>}/>
+                <Route path={PATH.PROFILE} element={<Profile/>}/>
+                <Route path={PATH.AUTH.LOGIN} element={<Login/>}/>
+                <Route path={PATH.AUTH.REGISTER} element={<Register/>}/>
+                <Route path={PATH.PASSWORD.RECOVERY} element={<PasswordRecovery/>}/>
+                <Route path={PATH.PASSWORD.CHANGE} element={<ChangePassword/>}/>
+                <Route path={PATH.E404} element={<E404/>}/>
+
+            </Routes>
         </div>
     );
 }
