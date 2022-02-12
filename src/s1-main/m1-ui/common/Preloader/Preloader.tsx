5e8acedd9ab0514ebe9df6221181a3dfd632f0ec
mykolao <mykolaoleskiv@gmail.com>
2022-02-12 21:05:52 +0200

    add styles

--- src/s1-main/m1-ui/common/Preloader/Preloader.tsx
+++ src/s1-main/m1-ui/common/Preloader/Preloader.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {CSSProperties} from 'react';
 import spinner from '../../../../assets/spinner.gif'
 
 type PreloaderPropsType = {
-    className?:string
+    className?: string
 }
+
+const spinnerStyle: CSSProperties = {
+    width: "100px",
+    height: "100px",
+    margin: "24px"
+}
+
+const containerStyle: CSSProperties = {
+    display: "flex",
+    justifyContent: "center"
+}
+
 const Preloader: React.FC<PreloaderPropsType> = ({className}) => {
-    return <div className={className}>
-        <img src={spinner} alt="loading animation"/>
+    return <div className={className} style={containerStyle}>
+        <img src={spinner} alt="loading animation" style={spinnerStyle}/>
     </div>
 
 
