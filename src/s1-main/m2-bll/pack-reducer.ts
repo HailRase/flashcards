46bd05137be8467c257fe04213914b9ec6aafcfe
mykolao <mykolaoleskiv@gmail.com>
2022-02-17 01:15:02 +0200

    create types

--- src/s1-main/m2-bll/pack-reducer.ts
+++ src/s1-main/m2-bll/pack-reducer.ts
@@ -1,3 +1,33 @@
+import { GetPacksParams, IPack } from "../m3-dal/pack";
+
+// Reducer State Type
+interface PackState {
+  status: PackStatus;
+  filter: PackFilter;
+  packs: IPack[];
+  packsTotal: number;
+  errorMessage?: string;
+}
+
 export const packReducer = (state: any, action: any): any => {
   return state;
 };
+
+// TYPES
+export type SortPacks =
+  | "0updated"
+  | "1updated"
+  | "0name"
+  | "1name"
+  | "0cardsCount"
+  | "1cardsCount";
+
+export interface PackFilter
+  extends Required<Omit<GetPacksParams, "sortPacks">> {
+  sortPacks: SortPacks;
+}
+export type PackStatus =
+  | "init"
+  | "loading"
+  | "loaded"
+  | "error";
