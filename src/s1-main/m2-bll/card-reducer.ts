7077832231ec4986322d6d0655b1654af5486696
mykolao <mykolaoleskiv@gmail.com>
2022-02-18 04:05:54 +0200

    create types

--- src/s1-main/m2-bll/card-reducer.ts
+++ src/s1-main/m2-bll/card-reducer.ts
@@ -1,4 +1,29 @@
 
+// Reducer State Type
+import {GetCardsParams, ICard} from "../m3-dal/card";
+
+interface CardState {
+    status: CardStatus;
+    filter: CardFilter;
+    cards: ICard[];
+    cardsTotal: number;
+    errorMessage?: string;
+}
+
 export const cardReducer = (state: any, action: any): any => {
     return state;
 }
+
+// Types
+type SortCards = "0updated" | "1updated" | "0grade" | "1grade";
+
+export interface CardFilter
+    extends Required<Omit<GetCardsParams, "sortCards">> {
+    sortCards: SortCards;
+}
+
+export type CardStatus =
+    | "init"
+    | "loading"
+    | "loaded"
+    | "error";
