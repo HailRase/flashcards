d93d8ff1515516e25dbf0e2dc022750a989656b1
mykolao <mykolaoleskiv@gmail.com>
2022-02-18 23:22:12 +0200

    init Sidebar component

---
+++ src/s2-features/f5-packs/p1-sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+import {FC} from "react";
+
+import s from "./sidebar.module.css"
+
+const Sidebar: FC = () => {
+    return <aside className={s.sidebar}>Sidebar</aside>
+}
+
+export default Sidebar;
