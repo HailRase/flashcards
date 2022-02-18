937f7fdf70c52428eec58eb11542685882ca0a18
mykolao <mykolaoleskiv@gmail.com>
2022-02-18 04:08:47 +0200

    create actions

--- src/s1-main/m2-bll/card-reducer.ts
+++ src/s1-main/m2-bll/card-reducer.ts
@@ -1,7 +1,60 @@
-
-// Reducer State Type
 import {GetCardsParams, ICard} from "../m3-dal/card";
 
+// Actions
+export const setCards = (cards: ICard[]) => {
+    return {
+        type: "CARD/SET_CARDS",
+        cards,
+    } as const;
+};
+
+type SetCards = ReturnType<typeof setCards>;
+
+export const setCardsTotalCount = (cardsTotal: number) => {
+    return {
+        type: "CARD/SET_CARDS_TOTAL",
+        cardsTotal
+    } as const;
+}
+
+type SetCardsTotalCount = ReturnType<typeof setCardsTotalCount>;
+
+export const setCardStatus = (status: CardStatus) => {
+    return {
+        type: "CARD/SET_STATUS",
+        status,
+    } as const;
+};
+
+type SetCardStatus = ReturnType<typeof setCardStatus>;
+
+export const setCardError = (errorMessage: string) => {
+    return {
+        type: "CARD/SET_ERROR",
+        errorMessage,
+    } as const;
+};
+
+type SetCardError = ReturnType<typeof setCardError>;
+
+export const setCardFilter = (filter: CardFilter) => {
+    return {
+        type: "CARD/SET_FILTER",
+        filter,
+    } as const;
+}
+
+type SetCardFilter = ReturnType<typeof setCardFilter>;
+
+export type CardAction =
+    | SetCards
+    | SetCardStatus
+    | SetCardError
+    | SetCardFilter
+    | SetCardsTotalCount;
+
+
+// Reducer State Type
 interface CardState {
     status: CardStatus;
     filter: CardFilter;
