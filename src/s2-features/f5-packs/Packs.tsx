9252d44301734041f855cc3649668aa6c765172b
mykolao <mykolaoleskiv@gmail.com>
2022-02-18 23:14:35 +0200

    style container

--- src/s2-features/f5-packs/Packs.tsx
+++ src/s2-features/f5-packs/Packs.tsx
@@ -4,13 +4,15 @@ import {Navigate} from "react-router-dom";
 import {PATH} from "../../s1-main/m1-ui/routes/routes";
 import {StoreType} from "../../s1-main/m2-bll/store";
 
+import s from "./packs.module.css"
+
 const Packs: FC = () => {
     const isAuth = useSelector<StoreType, boolean>(state => state.auth.isAuth);
 
 
     if (!isAuth) return <Navigate replace to={PATH.AUTH.LOGIN}/>
 
-    return <>Packs Page</>
+    return <div className={s.container}>Packs Page</div>
 }
 
 export default Packs
