79af4bfe39598a5a635b03654e06b6ec7497191c
mykolao <mykolaoleskiv@gmail.com>
2022-02-19 01:46:17 +0200

    create PacksHeader component

---
+++ src/s2-features/f5-packs/p2-packs-header/PacksHeader.tsx
@@ -0,0 +1,19 @@
+import {FC} from "react"
+
+import s from "./packs-header.module.css"
+import searchIcon from "../../../assets/search_icon.png";
+
+const PacksHeader: FC = () => {
+    return <div className={s.header}>
+        <h2 className={s.title}>Packs List</h2>
+        <div className={s.tools}>
+            <div className={s.search_container}>
+                <img src={searchIcon} alt="search packs"/>
+                <input className={s.input} type="text" name="Search..." placeholder="search"/>
+            </div>
+            <button className={s.button}>Add New Pack</button>
+        </div>
+    </div>
+}
+
+export default PacksHeader
