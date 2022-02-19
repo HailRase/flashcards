e6b03bdbfdf91ebb8408f511a0c654bdb7981893
mykolao <mykolaoleskiv@gmail.com>
2022-02-19 23:44:56 +0200

    complete delete my pack feature

--- src/s2-features/f5-packs/p3-packs-table/PacksTable.tsx
+++ src/s2-features/f5-packs/p3-packs-table/PacksTable.tsx
@@ -1,7 +1,8 @@
-import {FC} from "react";
-import {useSelector} from "react-redux";
-import {PackState} from "../../../s1-main/m2-bll/pack-reducer";
+import {FC, MouseEvent} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {deletePack, PackState} from "../../../s1-main/m2-bll/pack-reducer";
 import {StoreType} from "../../../s1-main/m2-bll/store";
+
 
 import s from "./packs-table.module.css"
 
@@ -29,7 +30,13 @@ const TableSpinner = () => {
 const PacksTable: FC = () => {
     const {filter, packs, status} = useSelector<StoreType, PackState>(state => state.pack)
     const id = useSelector<StoreType, string | undefined>(state => state.auth.userData?._id) || "";
+    const dispatch = useDispatch();
+
     const {pageCount} = filter;
+
+    const deletePackHandler = (e: MouseEvent<HTMLImageElement>) => {
+        dispatch(deletePack(e.currentTarget.dataset.id || ""))
+    }
 
     const Rows = status === "loaded" && packs.map((pack, i) => {
         return (<div key={pack._id} className={`${s.row} ${(i % 2) && s.dark}`}>
@@ -38,7 +45,12 @@ const PacksTable: FC = () => {
             <div className={s.col_2}>{pack.updated.substring(0, 10)}</div>
             <div className={s.col_3}>{pack.user_name}</div>
             <div className={s.col_4}>{pack.user_id === id && <>
-                <img src={deleteIcon} alt="delete my pack" className={`${s.icon} ${s.icon_red}`}/>
+                <img src={deleteIcon}
+                     alt="delete my pack"
+                     data-id={pack._id}
+                     onClick={deletePackHandler}
+                     className={`${s.icon} ${s.icon_red}`}/>
+
                 <img src={editIcon} alt="edit my pack" className={`${s.icon} ${s.icon_blue}`}/>
             </>}
                 <img src={eyeIcon} alt="view cards" className={`${s.icon} ${s.icon_blue}`}/>
