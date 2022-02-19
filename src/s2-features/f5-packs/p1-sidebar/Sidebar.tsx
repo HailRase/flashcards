f1d36550776994baa01f1d55902c64dcf151ab06
mykolao <mykolaoleskiv@gmail.com>
2022-02-19 04:23:29 +0200

    complete select my cards feature

--- src/s2-features/f5-packs/p1-sidebar/Sidebar.tsx
+++ src/s2-features/f5-packs/p1-sidebar/Sidebar.tsx
@@ -1,25 +1,33 @@
-import {FC, MouseEvent, useState} from "react";
+import {FC} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchPacks, PackFilter} from "../../../s1-main/m2-bll/pack-reducer";
+import {StoreType} from "../../../s1-main/m2-bll/store";
 
 import s from "./sidebar.module.css"
 
 const Sidebar: FC = () => {
-    const [showCards, setShowCards] = useState<"all" | "my">("all");
+    const id = useSelector<StoreType, string>(state => state.auth.userData?._id || "");
+    const filter = useSelector<StoreType, PackFilter>(state => state.pack.filter);
+    const dispatch = useDispatch();
+
+    const filterId = filter.user_id;
+    const cards = id.length && id === filterId ? "my" : "all";
 
     const onSelect = (select: string) => {
-        if (showCards === "all" && select === "my") {
-            setShowCards("my");
+        if (cards === "all" && select === "my") {
+            dispatch(fetchPacks({...filter, user_id: id}));
         }
 
-        if (showCards === "my" && select === "all") {
-            setShowCards("all");
+        if (cards === "my" && select === "all") {
+            dispatch(fetchPacks({...filter, user_id: ""}));
         }
     }
 
     return <aside className={s.sidebar}>
         <div className={s.title}>Show Packs</div>
         <div className={s.select_container}>
-            <div className={`${s.select} ${showCards === "my" && s.selected}`} onClick={() => onSelect("my")}>My</div>
-            <div className={`${s.select} ${showCards === "all" && s.selected}`} onClick={() => onSelect("all")}>All
+            <div className={`${s.select} ${cards === "my" && s.selected}`} onClick={() => onSelect("my")}>My</div>
+            <div className={`${s.select} ${cards === "all" && s.selected}`} onClick={() => onSelect("all")}>All
             </div>
         </div>
         <div className={s.title}>Number of cards</div>
