5bfd83a6bc11b89325008a036ccd7e2da2b39fe7
mykolao <mykolaoleskiv@gmail.com>
2022-02-19 04:31:58 +0200

    complete PacksTable component

---
+++ src/s2-features/f5-packs/p3-packs-table/PacksTable.tsx
@@ -0,0 +1,84 @@
+import {FC} from "react";
+import {useSelector} from "react-redux";
+import {PackState} from "../../../s1-main/m2-bll/pack-reducer";
+import {StoreType} from "../../../s1-main/m2-bll/store";
+
+import s from "./packs-table.module.css"
+
+import spinner from "../../../assets/spinner.gif";
+
+const TableHeader = () => {
+    return <div className={s.header}>
+
+        <div className={s.col_0}>Name</div>
+        <div className={s.col_1}>Cards</div>
+        <div className={s.col_2}>Last Updated</div>
+        <div className={s.col_3}>Created By</div>
+        <div className={s.col_4}>Actions</div>
+
+    </div>
+}
+
+const EmptyRow = (i: number) => {
+    return <div key={i} className={`${s.row} ${i % 2 && s.dark}`}>
+        <div className={s.col_0}> </div>
+        <div className={s.col_1}> </div>
+        <div className={s.col_2}> </div>
+        <div className={s.col_3}> </div>
+        <div className={s.col_4}> </div>
+    </div>
+}
+
+const TableSpinner = () => {
+    return <img className={s.spinner} src={spinner} alt="loading spinner"/>
+}
+
+const PacksTable: FC = () => {
+    const {filter, packs, status} = useSelector<StoreType, PackState>(state => state.pack)
+    const id = useSelector<StoreType, string | undefined>(state => state.auth.userData?._id) || "";
+    const {pageCount} = filter;
+
+    const Rows = status === "loaded" && packs.map((pack, i) => {
+        return (<div key={pack._id} className={`${s.row} ${(i % 2) && s.dark}`}>
+            <div className={s.col_0}>{pack.name}</div>
+            <div className={s.col_1}>{pack.cardsCount}</div>
+            <div className={s.col_2}>{pack.updated.substring(0, 10)}</div>
+            <div className={s.col_3}>{pack.user_name}</div>
+            <div className={s.col_4}>{pack.user_id === id && <>
+                <button>e</button>
+                <button>x</button>
+            </>}
+                <button>learn</button>
+            </div>
+        </div>)
+    });
+
+    // Create empty rows
+    let EmptyRows: JSX.Element[] = [];
+    if (pageCount > packs.length) {
+        if (!packs.length) {
+            for (let i = 0; i < pageCount - packs.length; i++) {
+                EmptyRows.push(EmptyRow(i))
+            }
+        } else {
+            for (let i = pageCount - packs.length; i < pageCount; i++) {
+                EmptyRows.push(EmptyRow(i))
+            }
+        }
+    }
+
+    if (status !== "loaded") {
+        return <div className={s.table}>
+            <TableHeader/>
+            <TableSpinner/>
+        </div>
+    }
+
+    return <div className={s.table}>
+        <TableHeader/>
+        {Rows}
+        {EmptyRows}
+    </div>
+}
+
+export default PacksTable;
