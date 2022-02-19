1ff93ec37c546562caf5ed3621a9959cb29b6d41
mykolao <mykolaoleskiv@gmail.com>
2022-02-19 22:05:51 +0200

    complete search feature

--- src/s2-features/f5-packs/p2-packs-header/PacksHeader.tsx
+++ src/s2-features/f5-packs/p2-packs-header/PacksHeader.tsx
@@ -1,15 +1,35 @@
-import {FC} from "react"
+import {ChangeEvent, FC, useEffect, useState} from "react"
 
 import s from "./packs-header.module.css"
 import searchIcon from "../../../assets/search_icon.png";
+import {useDispatch, useSelector} from "react-redux";
+import {StoreType} from "../../../s1-main/m2-bll/store";
+import {fetchPacks, PackState} from "../../../s1-main/m2-bll/pack-reducer";
 
 const PacksHeader: FC = () => {
+    const [searchValue, setSearchValue] = useState("");
+    const {status, filter} = useSelector<StoreType, PackState>(state => state.pack);
+    const dispatch = useDispatch();
+
+    const storedSearchValue = filter.packName;
+
+    useEffect(() => {
+        if (status === "loaded" && storedSearchValue !== searchValue) {
+            dispatch(fetchPacks({...filter, packName: searchValue}));
+        }
+    }, [dispatch, filter, status, searchValue])
+
+    const onSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.currentTarget.value);
+    }
+
     return <div className={s.header}>
         <h2 className={s.title}>Packs List</h2>
         <div className={s.tools}>
             <div className={s.search_container}>
                 <img src={searchIcon} alt="search packs"/>
-                <input className={s.input} type="text" name="search" placeholder="Search..."/>
+                <input className={s.input} type="text" value={searchValue} onChange={onSearchChange} name="search"
+                       placeholder="Search..."/>
             </div>
             <button className={s.button}>Add New Pack</button>
         </div>
