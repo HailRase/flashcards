843bc46886c4e497888e65d1e9e59186dc44a194
mykolao <mykolaoleskiv@gmail.com>
2022-02-19 02:25:57 +0200

    complete sidebar select

--- src/s2-features/f5-packs/p1-sidebar/Sidebar.tsx
+++ src/s2-features/f5-packs/p1-sidebar/Sidebar.tsx
@@ -1,9 +1,29 @@
-import {FC} from "react";
+import {FC, MouseEvent, useState} from "react";
 
 import s from "./sidebar.module.css"
 
 const Sidebar: FC = () => {
-    return <aside className={s.sidebar}>Sidebar</aside>
+    const [showCards, setShowCards] = useState<"all" | "my">("all");
+
+    const onSelect = (select: string) => {
+        if (showCards === "all" && select === "my") {
+            setShowCards("my");
+        }
+
+        if (showCards === "my" && select === "all") {
+            setShowCards("all");
+        }
+    }
+
+    return <aside className={s.sidebar}>
+        <div className={s.title}>Show Packs</div>
+        <div className={s.select_container}>
+            <div className={`${s.select} ${showCards === "my" && s.selected}`} onClick={() => onSelect("my")}>My</div>
+            <div className={`${s.select} ${showCards === "all" && s.selected}`} onClick={() => onSelect("all")}>All
+            </div>
+        </div>
+        <div className={s.title}>Number of cards</div>
+    </aside>
 }
 
 export default Sidebar;
