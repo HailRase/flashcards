6c3e542490e354433941c4bd46bf0b6933cc49fa
mykolao <mykolaoleskiv@gmail.com>
2022-02-19 04:35:01 +0200

    add PacksHeader and PacksTable components

--- src/s2-features/f5-packs/Packs.tsx
+++ src/s2-features/f5-packs/Packs.tsx
@@ -1,18 +1,35 @@
-import {FC} from "react";
-import {useSelector} from "react-redux";
+import {FC, useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
 import {Navigate} from "react-router-dom";
 import {PATH} from "../../s1-main/m1-ui/routes/routes";
+import {fetchPacks, PackState} from "../../s1-main/m2-bll/pack-reducer";
 import {StoreType} from "../../s1-main/m2-bll/store";
+import Sidebar from "./p1-sidebar/Sidebar";
+import PacksHeader from "./p2-packs-header/PacksHeader";
+import PacksTable from "./p3-packs-table/PacksTable";
 
 import s from "./packs.module.css"
 
 const Packs: FC = () => {
     const isAuth = useSelector<StoreType, boolean>(state => state.auth.isAuth);
+    const {status, filter} = useSelector<StoreType, PackState>(state => state.pack);
+    const dispatch = useDispatch();
 
+    useEffect(() => {
+        if (status === "init" && isAuth) {
+            dispatch(fetchPacks(filter));
+        }
+    }, [status, isAuth, dispatch, filter])
 
     if (!isAuth) return <Navigate replace to={PATH.AUTH.LOGIN}/>
 
-    return <div className={s.container}>Packs Page</div>
+    return <div className={s.container}>
+        <Sidebar/>
+        <main className={s.main}>
+            <PacksHeader/>
+            <PacksTable/>
+        </main>
+    </div>
 }
 
 export default Packs
