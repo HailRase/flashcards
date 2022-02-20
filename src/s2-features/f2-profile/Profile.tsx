8a888868bcc35436594ddc6948d0335660943b97
VadymHromyk <hromuk.vadim@gmail.com>
2022-02-20 17:34:28 +0200

    added profile

--- src/s2-features/f2-profile/Profile.tsx
+++ src/s2-features/f2-profile/Profile.tsx
@@ -1,23 +1,59 @@
 import React from 'react';
 import {useAppSelector} from "../../s1-main/m2-bll/store";
-import {Navigate} from "react-router-dom";
+import searchIcon from "../../assets/search_icon.png";
 
+import s from './Profile.module.css'
 
 const Profile = () => {
+
     const {userData, isAuth} = useAppSelector(state => state.auth)
-    if (!isAuth){
-        return <Navigate to={'/login'}/>
-    }
+    
+    // if (!isAuth){
+    //     return <Navigate to={'/login'}/>
+    // }
+
+    // userData?._id .name .email .avatar .publicCardPacksCount 
+
+    return <div className={s.container}>
+        <ProfileSidebar/>
+        <main className={s.main}>
+            <ProfileHeader/>
+            {/* <ProfileTable/>
+            {maxPageNumber > 1 && <Pagination currentPage={filter.page}
+                         maxPage={maxPageNumber}
+                         onChange={onPageChange}
+                         itemsPerPage={itemsPerPage}/>} */}
+        </main>
+    </div>
+};
+export default Profile;
+
+const ProfileSidebar = () => {
     return (
-        <div>
-            <div> {userData?._id}</div>
-            <div> {userData?.name}</div>
-            <div> {userData?.email}</div>
-            <div> {userData?.avatar}</div>
-            <div> {userData?.publicCardPacksCount}</div>
-            <h1>PROFILE PAGE!</h1>
+    <div className={s.profileSidebar}>
+        <div className={s.profileInfo}>
+            <img src="https://www.kindpng.com/picc/m/207-2074624_white-gray-circle-avatar-png-transparent-png.png" />
+            <div className={s.profileName}>Name Name</div>
+            <div className={s.profileWork}>Front-end developer</div>
+            <button>Edit profile</button>
         </div>
-    );
+
+        <div className={''}>
+            <div className={s.title}>Number of cards</div>
+            <br/>
+            <span>4---------100</span>
+        </div>
+    </div>)
 };
 
-export default Profile;
+const ProfileHeader = () => {
+    return (
+    <div className={''}>
+        <h2>My packs list</h2>
+            <div className={s.search_container}>
+                <img src={searchIcon} />
+                <input className={s.input} type="text" /*value={searchValue} onChange={onSearchChange}*/ name="search"
+                       placeholder="Search..."/>
+            </div>
+    </div>)
+};
