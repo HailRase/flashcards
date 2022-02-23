0ba9b784c4e47b73f9932e52b6a927b744679f91
Юра <yurii_klimko@mail.ru>
2022-02-23 16:59:37 +0300

    added cards search

--- src/s2-features/f6-cards/c1-cards-header/CardsHeader.tsx
+++ src/s2-features/f6-cards/c1-cards-header/CardsHeader.tsx
@@ -1,15 +1,33 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import s from './CardsHeader.module.css'
 import sph from './../../f5-packs/p2-packs-header/packs-header.module.css'
 import searchIcon from "../../../assets/search_icon.png";
 import {useNavigate, useParams} from "react-router-dom";
-import {useAppSelector} from "../../../s1-main/m2-bll/store";
 import CardEditor from "../c4-card-editor/CardEditor";
+import {useAppSelector} from "../../../s1-main/m2-bll/store";
+import {useDispatch} from "react-redux";
+import {fetchCards} from "../../../s1-main/m2-bll/card-reducer";
 
 const CardsHeader = () => {
-
+    const [searchValue, setSearchValue] = useState("");
     const [modeCardEditor, setModeCardEditor] = useState(false)
+    const params = useParams()
+    const dispatch = useDispatch()
+    const pack = useAppSelector(state => state.pack.packs.find(p => p._id === params.packCardsId))
+    const {status, filter} = useAppSelector(state => state.card)
     const navigate = useNavigate()
+
+    const storedSearchValue = filter.cardQuestion;
+
+    useEffect(() => {
+        if (status === "loaded" && storedSearchValue !== searchValue) {
+            dispatch(fetchCards({...filter, cardQuestion: searchValue}));
+        }
+    }, [dispatch, filter, status, searchValue])
+
+    const onSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.currentTarget.value);
+    }
     const onPacksNavigate = () => {
         navigate('/packs')
     }
@@ -22,7 +40,7 @@ const CardsHeader = () => {
                 <div className={s.arrowBack}>
                     <label className={s.back} onClick={onPacksNavigate}>
                         &larr;
-                        Pack name
+                        {" " + pack?.name}
                     </label>
                 </div>
                 <div className={s.searchBlock}>
@@ -30,6 +48,7 @@ const CardsHeader = () => {
                         <img src={searchIcon} alt="search packs"/>
                         <input className={sph.input} type="text"
                                name="search"
+                               onChange={onSearchChange}
                                placeholder="Search..."/>
                     </div>
                     <button className={sph.button} onClick={onModeChange}>Add Card</button>
