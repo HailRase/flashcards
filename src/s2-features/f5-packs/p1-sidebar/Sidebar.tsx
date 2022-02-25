e20b15cbf69109a5b4b69f7f95978866a6c2665d
mykolao <mykolaoleskiv@gmail.com>
2022-02-25 12:10:53 +0200

    fix Sidebar DoubleRange dispatch

--- src/s2-features/f5-packs/p1-sidebar/Sidebar.tsx
+++ src/s2-features/f5-packs/p1-sidebar/Sidebar.tsx
@@ -1,17 +1,33 @@
-import {FC} from "react";
+import {FC, useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {fetchPacks, PackFilter} from "../../../s1-main/m2-bll/pack-reducer";
+import {fetchPacks, PackFilter, PackStatus} from "../../../s1-main/m2-bll/pack-reducer";
 import {StoreType} from "../../../s1-main/m2-bll/store";
 import s from "./sidebar.module.css"
-import DoubleRange from "../../../s1-main/m1-ui/common/DoubleRange/DoubleRange";
+import DoubleRange, {
+    RangeValue
+} from "../../../s1-main/m1-ui/common/DoubleRange/DoubleRange";
 
 const Sidebar: FC = () => {
     const id = useSelector<StoreType, string>(state => state.auth.userData?._id || "");
     const filter = useSelector<StoreType, PackFilter>(state => state.pack.filter);
+    const status = useSelector<StoreType, PackStatus>(state => state.pack.status);
     const dispatch = useDispatch();
 
+    // Local Range Logic
+    const [localRange, setLocalRange] = useState<RangeValue>({min: filter.min, max: filter.max});
+
+    useEffect(() => {
+        const {min, max} = localRange;
+        const updateFilter = (min !== filter.min) || (max !== filter.max);
+        if(updateFilter && status === "loaded") {
+            dispatch(fetchPacks({...filter, min, max}));
+        }
+    }, [filter, localRange, dispatch, status]);
+
+    // My/All Cards Logic
     const filterId = filter.user_id;
     const cards = id.length && id === filterId ? "my" : "all";
+
 
     const onSelect = (select: string) => {
         if (cards === "all" && select === "my") {
@@ -35,7 +51,7 @@ const Sidebar: FC = () => {
         </div>
         <div className={s.doubleRangeContainer}>
             <div className={s.title}>Number of cards</div>
-            <DoubleRange min={1} max={150} onChange={value => 10}/>
+            <DoubleRange min={0} max={150} onChange={setLocalRange}/>
         </div>
 
     </aside>
