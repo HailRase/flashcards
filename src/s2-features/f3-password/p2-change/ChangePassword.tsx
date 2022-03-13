0c34b55d047d0ee9a5a3d3bfa238623da6c438ef
Юра <yurii_klimko@mail.ru>
2022-03-13 21:02:38 +0300

    refactor change password page

--- src/s2-features/f3-password/p2-change/ChangePassword.tsx
+++ src/s2-features/f3-password/p2-change/ChangePassword.tsx
@@ -1,18 +1,11 @@
 import SuperInputText from "../../../s1-main/m1-ui/common/c1-SuperInputText/SuperInputText";
 import SuperButton from "../../../s1-main/m1-ui/common/c2-SuperButton/SuperButton";
-
 import {PATH} from "../../../s1-main/m1-ui/routes/routes";
-
-import {
-    changePassword,
-    ResetPasswordStatus,
-} from "../../../s1-main/m2-bll/password-reducer";
+import {changePassword, ResetPasswordStatus,} from "../../../s1-main/m2-bll/password-reducer";
 import {useDispatch, useSelector} from "react-redux";
 import {StoreType} from "../../../s1-main/m2-bll/store";
-
-import {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 import {Navigate, useParams} from "react-router-dom";
-
 import s from "./ChangePassword.module.css";
 
 type URLParams = {
@@ -26,23 +19,16 @@ const selectStatus = (
 };
 
 const ChangePassword = () => {
-    // Global State
     const dispatch = useDispatch();
     const status = useSelector(selectStatus);
-
-    // Local State
     const [newPassword, setNewPassword] = useState("");
     const {resetPasswordToken} = useParams<URLParams>();
 
-    // Redirect to Login Page
     if (status === "success") {
         return (<Navigate to={PATH.AUTH.LOGIN}/>);
     }
 
-    // Events
-    const onNewPasswordChange = (
-        e: ChangeEvent<HTMLInputElement>
-    ) => {
+    const onNewPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
         setNewPassword(e.currentTarget.value);
     };
 
@@ -54,17 +40,18 @@ const ChangePassword = () => {
     return (
         <div className={s.wrapper}>
             <div className={s.container}>
-                <h2 className={s.logo}>it-incubator</h2>
-                <h2 className={s.title}>Create New Password</h2>
-                <SuperInputText
-                    type="password"
-                    value={newPassword}
-                    onChange={onNewPasswordChange}
-                />
-                <SuperButton onClick={onSubmit}>
-                    Change Password
-                </SuperButton>
-                {status === "error" && (<p className={s.error}>An Error Occurred!</p>)}
+                <span className={s.changeTitle} style={{fontSize: '24px'}}>Flashcards</span>
+                <span className={s.changeTitle}>Create new password</span>
+                <div className={s.changeFields}>
+                    <div className={s.changeField}>
+                        <input className={s.fieldInput}
+                               onChange={onNewPasswordChange}
+                               placeholder={' '}/>
+                        <label className={s.fieldTitle}>Password</label>
+                    </div>
+                    <span className={s.info}>Create new password and we will send you further instructions to email</span>
+                </div>
+                <button className={s.changeButton} onClick={onSubmit}>Change Password</button>
             </div>
         </div>
     );
